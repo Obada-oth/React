b9bf29b81fc599496bdb38fa3f04a618e9d3af00
obada othman <obada.oth@gmail.com>
2020-11-24 22:59:44 +0100

    hackyourweather w3 done

---
+++ week1/project/hackyourweather/src/Search/Search.js
@@ -0,0 +1,29 @@
+import "./Search.css";
+const Search = ({ city, setCity, fetchWeatherData }) => {
+  return (
+    <div className="search-container">
+      <div className="search-field">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+          placeholder="Search City"
+        />
+      </div>
+      <div className="search-button">
+        <button
+          type="submit"
+          disabled={!city}
+          onClick={() => {
+            fetchWeatherData(city);
+          }}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+export default Search;
